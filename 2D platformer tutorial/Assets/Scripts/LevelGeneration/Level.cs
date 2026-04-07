3788e60a618404765f5cf5c8f398ad9473a2fc3a
Gargis <bokaj712@gmail.com>
2026-04-07 17:23:05 +0200

    Pushat lite ändringar

--- 2D platformer tutorial/Assets/Scripts/LevelGeneration/Level.cs
+++ 2D platformer tutorial/Assets/Scripts/LevelGeneration/Level.cs
@@ -88,7 +88,7 @@ public class Level
         firstPath.Add(entrance);
 
         int steps = 0;
-        int maxSteps = 12;
+        int maxSteps = 8;
         int stuckCount = 0;
 
         while (steps < maxSteps)
@@ -173,7 +173,7 @@ public class Level
                 int moveAttempts = 0;
                 bool moved = false;
 
-                while (!moved && moveAttempts < 20)
+                /*while (!moved && moveAttempts < 20)
                 {
                     moveAttempts++;
                     switch (RandomDirection())
@@ -200,6 +200,37 @@ public class Level
                             if (x > 0
                                 && rooms[GetRoomID(x - 1, y)].Type == 0
                                 && !HasMultipleNeighbours(x - 1, y, x, y))
+                            { x--; rooms[GetRoomID(x, y)].Type = 5; moved = true; }
+                            break;
+                    }
+                }*/
+                while (!moved && moveAttempts < 20)
+                {
+                    moveAttempts++;
+                    switch (RandomDirection())
+                    {
+                        case Direction.RIGHT:
+                            if (x < width - 1
+                                && rooms[GetRoomID(x + 1, y)].Type == 0
+                                && (!HasMultipleNeighbours(x + 1, y, x, y) || Random.value < 0.002f))
+                            { x++; rooms[GetRoomID(x, y)].Type = 2; moved = true; }
+                            break;
+                        case Direction.UP:
+                            if (y > 0
+                                && rooms[GetRoomID(x, y - 1)].Type == 0
+                                && (!HasMultipleNeighbours(x, y - 1, x, y) || Random.value < 0.002f))
+                            { y--; rooms[GetRoomID(x, y)].Type = 3; moved = true; }
+                            break;
+                        case Direction.DOWN:
+                            if (y < height - 1
+                                && rooms[GetRoomID(x, y + 1)].Type == 0
+                                && (!HasMultipleNeighbours(x, y + 1, x, y) || Random.value < 0.002f))
+                            { y++; rooms[GetRoomID(x, y)].Type = 4; moved = true; }
+                            break;
+                        case Direction.LEFT:
+                            if (x > 0
+                                && rooms[GetRoomID(x - 1, y)].Type == 0
+                                && (!HasMultipleNeighbours(x - 1, y, x, y) || Random.value < 0.002f))
                             { x--; rooms[GetRoomID(x, y)].Type = 5; moved = true; }
                             break;
                     }
